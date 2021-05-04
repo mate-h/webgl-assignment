f8461b7f77c37391e55b5734d3e4a5526908f0dc
Máté Homolya <mate.homolya@gmail.com>
2021-05-04 12:33:23 -0600

    Add specular in gouraud shader

--- shaders/gouraud/vertex.glsl
+++ shaders/gouraud/vertex.glsl
@@ -20,9 +20,19 @@ void main(void) {
   vec3 normal = normalize(vTransformedNormal);
   float diffuseLightWeight = max(dot(normal, lightDirection), 0.0);
   float diffuseLightIntensity = 1.0;
-  vec4 diffuseLightColor = vec4(1.0,1.0,1.0,1.0) * diffuseLightIntensity;
-  float ambientLightIntensity = 0.2;
-  vec4 ambientLightColor = vec4(1.0,1.0,1.0,1.0) * ambientLightIntensity;
-  vec4 lightWeighting = ambientLightColor + diffuseLightColor * diffuseLightWeight;
+  vec4 diffuseLightColor = vec4(diffuseLightIntensity * diffuseLightWeight);
+  float ambientLightIntensity = 0.1;
+  vec4 ambientLightColor = vec4(ambientLightIntensity);
+
+  float materialShininess = 1.0;
+  float specularLightWeighting = 0.0;
+  vec3 eyeDirection = normalize(-vPosition.xyz);
+  vec3 reflectionDirection = reflect(-lightDirection, normal);
+
+  specularLightWeighting = pow(max(dot(reflectionDirection, eyeDirection), 0.0), materialShininess);
+  float specularLightIntensity = 0.4;
+  vec4 specularLightColor = vec4(specularLightWeighting * specularLightIntensity);
+
+  vec4 lightWeighting = ambientLightColor + diffuseLightColor + specularLightColor;
   fragcolor = vec4(aFrontColor.rgb * lightWeighting.rgb, 1.0);
 }
